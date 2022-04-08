b148bae4d6d58ae008400e73fd515416a330c404
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-08 17:36:43 +0000

    refactor: using composition as an events refactor

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ id: 4, name: 'Gaster', age: 40 });
 
-user.set({ name: 'Alfred' });
+user.events.on('change', () => {
+  console.log('change!!');
+});
 
-// const user = new User({ id: 2, name: 'John', age: 10 });
-
-user.save();
-
-user.fetch();
-
-const otherUser = new User({ id: 2, name: 'John', age: 10 });
-
-otherUser.set({ name: 'Alfred' });
-
-// const otherUser = new User({ id: 2, name: 'John', age: 10 });
-
-otherUser.save();
-
-// otherUser.fetch();
+user.events.trigger('change');
