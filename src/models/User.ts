da94c73417cab86d4a5264918499ccacf65bf45b
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-08 17:25:55 +0000

    refactor: event logic from user.ts to separate class

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,15 +6,7 @@ interface UserProps {
   age?: number;
 }
 
-interface Events {
-  [key: string]: Callback[];
-}
-
-type Callback = () => void;
-
 export class User {
-  events: Events = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -23,24 +15,6 @@ export class User {
 
   set(updatedData: UserProps): void {
     Object.assign(this.data, updatedData);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-
-    handlers.push(callback);
-
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) return;
-
-    handlers.forEach(callback => {
-      callback();
-    });
   }
 
   async fetch(): Promise<void> {
