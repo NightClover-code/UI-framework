a66b9a7ffbce56fe7eb941a57ae36f2c671cefe5
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-08 18:20:14 +0000

    refactor: adding attributes class

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,14 +10,4 @@ interface UserProps {
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(updatedData: UserProps): void {
-    Object.assign(this.data, updatedData);
-  }
 }
