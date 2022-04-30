2b242b7f05c23b95b3a6c4565063ace32e8103be
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-30 04:10:39 +0000

    feat: final model touches

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,20 +1,75 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
 import { Callback } from './Eventing';
 
-interface ModelAttrs<T> {
-  set: (updatedData: T) => void;
+interface Required {
+  id?: number;
 }
 
-interface Sync {}
+interface ModelAttrs<T> {
+  get: <K extends keyof T>(key: K) => T[K];
+  set: (updatedData: T) => void;
+  getAll: () => T;
+}
+
+interface Sync<T> {
+  fetch: (id: number) => AxiosPromise;
+  save: (data: T) => AxiosPromise;
+}
 
 interface Events {
   on: (eventName: string, callback: Callback) => void;
   trigger: (eventName: string) => void;
 }
 
-export class Model<T> {
+export class Model<T extends Required> {
   constructor(
     private attrs: ModelAttrs<T>,
     private events: Events,
-    private sync: Sync
+    private sync: Sync<T>
   ) {}
+
+  /*
+    using accessors to easily reach other 
+    class methods by composition
+  */
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attrs.get;
+  }
+
+  set(updatedData: T): void {
+    this.attrs.set(updatedData);
+
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (!id) throw new Error('Cannot fetch without ID');
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
+
+  save(): void {
+    const data = this.attrs.getAll();
+
+    this.sync
+      .save(data)
+      .then((res: AxiosResponse) => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { AxiosResponse } from 'axios';
-import { Attributes } from './Attributes';
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 
 interface UserProps {
   id?: number;
@@ -9,56 +6,4 @@ interface UserProps {
   age?: number;
 }
 
-export class User {
-  private events: Eventing = new Eventing();
-  private sync: Sync<UserProps> = new Sync<UserProps>();
-  private attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-  /*
-    using accessors to easily reach other 
-    class methods by composition
-  */
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(updatedData: UserProps): void {
-    this.attributes.set(updatedData);
-
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    if (!id) throw new Error('Cannot fetch without ID');
-
-    this.sync.fetch(id).then((res: AxiosResponse): void => {
-      this.set(res.data);
-    });
-  }
-
-  save(): void {
-    const data = this.attributes.getAll();
-
-    this.sync
-      .save(data)
-      .then((res: AxiosResponse) => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
-  }
-}
+export class User extends Model<UserProps> {}
